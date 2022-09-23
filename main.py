21a4119d10b1481f760ffa0448e1964b641bec64
jitendra kalyan <pjay@jitendras-MacBook-Air.local>
2022-09-23 10:35:49 +0530

    addded prod run config

--- main.py
+++ main.py
@@ -40,4 +40,6 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host = '0.0.0.0', port = port, debug=True)
+    # app.run(debug=True)
